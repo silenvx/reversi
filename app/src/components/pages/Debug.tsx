308c7a409c1e1b417839f09e288c8a1b71b8cb77
silenvx <czvziwzx@gmail.com>
2024-06-25 03:22:12 +0900

    fix: npm run build

--- app/src/components/pages/Debug.tsx
+++ app/src/components/pages/Debug.tsx
@@ -1,13 +1,3 @@
-import { HandContainer } from "@/components/features/hand/HandContainer";
-import { Disc } from "@/domains/reversi/const";
-
 export function Debug() {
-  return (
-    <div className="flex justify-between">
-      <HandContainer player={Disc.black} />
-      <HandContainer player={Disc.black} />
-      <HandContainer player={Disc.black} />
-      <HandContainer player={Disc.black} />
-    </div>
-  );
+  return <div>debug</div>;
 }
