ccfb334c7ff9310d1f447629d0a6c3a98b054061
silenvx <czvziwzx@gmail.com>
2024-07-06 16:09:01 +0900

    fix: 現在のプレイヤーはスコアが光る

--- app/src/components/features/score/ScorePresenter.tsx
+++ app/src/components/features/score/ScorePresenter.tsx
@@ -1,22 +1,27 @@
 import { Disc, DiscType } from "@/domains/reversi/const";
 
-const colorClass = (player: DiscType) => {
+const colorClass = (player: DiscType, isPlaying: boolean) => {
   if (player === Disc.black) {
-    return "bg-red-200/10 border-red-500/50 text-red-500";
+    return `text-red-500 ${isPlaying ? "bg-red-200/20 border-red-500/100 shadow-red-200 shadow-sm" : "bg-red-200/10 border-red-500/50"}`;
   }
-  return "bg-blue-200/10 border-blue-500/50 text-blue-500";
+  return `text-blue-500 ${isPlaying ? "bg-blue-200/20 border-blue-500/100 shadow-blue-200 shadow-sm" : "bg-blue-200/10 border-blue-500/50"}`;
 };
 
 type ScoreContainerProps = {
   score: number;
   player: DiscType;
+  isPlaying: boolean;
 };
 
-export function ScorePresenter({ score, player }: ScoreContainerProps) {
+export function ScorePresenter({
+  score,
+  player,
+  isPlaying,
+}: ScoreContainerProps) {
   return (
     <div>
       <div
-        className={`mx-auto flex h-52 w-52 items-center justify-center rounded-full border-4 text-5xl shadow-lg backdrop-blur-lg ${colorClass(player)}`}
+        className={`mx-auto flex h-52 w-52 items-center justify-center rounded-full border-4 text-5xl shadow-lg backdrop-blur-lg ${colorClass(player, isPlaying)}`}
       >
         {score}
       </div>
