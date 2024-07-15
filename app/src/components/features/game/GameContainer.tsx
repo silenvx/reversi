0319f4850eeb01d8e1c251b9233a5b5adcf372c3
silenvx <czvziwzx@gmail.com>
2024-07-16 00:17:08 +0900

    fix: 途中で勝ったときにバグらないようにした

--- app/src/components/features/game/GameContainer.tsx
+++ app/src/components/features/game/GameContainer.tsx
@@ -2,21 +2,23 @@ import { useEffect } from "react";
 
 import { GamePresenter } from "@/components/features/game/GamePresenter";
 import { Winner } from "@/domains/reversi/const";
+import { useHandContext } from "@/hooks/handContext";
 import { useReversiGame } from "@/hooks/reversiGame";
 
 export function GameContainer() {
   const reversiGame = useReversiGame();
+  const { resetCard } = useHandContext();
 
   // 勝者が決まったらアラートを表示してリセット
   useEffect(() => {
     switch (reversiGame.winner) {
       case Winner.black:
         // eslint-disable-next-line no-alert
-        alert("黒の勝ちです");
+        alert("赤の勝ちです");
         break;
       case Winner.white:
         // eslint-disable-next-line no-alert
-        alert("白の勝ちです");
+        alert("青の勝ちです");
         break;
       case Winner.draw:
         // eslint-disable-next-line no-alert
@@ -26,6 +28,7 @@ export function GameContainer() {
         break;
     }
     reversiGame.reset();
+    resetCard();
   }, [reversiGame.winner]);
 
   return <GamePresenter reversiGame={reversiGame} />;
